6550658c50c6d409173e5739788208c9f413efb6
AleHajSar <2022-102924@rtu.edu.ph>
2026-01-23 23:01:05 +0800

    Update App.jsx

--- Ali/src/App.jsx
+++ Ali/src/App.jsx
@@ -2,9 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Applicant_SignUp from "./Applicant_SignUp";
 import Login_Applicant from "./Login_Applicant";
 import Reset_Password from "./Reset_Password";
+import Confirm_Reset from "./Confirm_Reset";
 import Set_New_Password from "./Set_New_Password";
 import Submit_Application from "./Submit_Application";
 import My_Applications from "./My_Applications";
+
 
 export default function App() {
   return (
@@ -12,6 +14,7 @@ export default function App() {
       <Route path="/" element={<Applicant_SignUp />} />
       <Route path="/login-applicant" element={<Login_Applicant />} />
       <Route path="/reset_password" element={<Reset_Password />} />
+      <Route path="/confirm-reset" element={<Confirm_Reset />} />
       <Route path="/set-new-password" element={<Set_New_Password />} />
       <Route path="/submit-application" element={<Submit_Application />} />
       <Route path="/my_applications" element={<My_Applications />} />
