10de5512b01e194d6971e983a060a1b373dddfcb
tapsiloooog <252837749+tapsiloooog@users.noreply.github.com>
2026-02-01 14:13:28 +0800

    Add files via upload
    added files on compiled v2 fldr

---
+++ compiled v.2/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // add these only if they exist in your project:
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
